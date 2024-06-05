b746fe12982011ad71cba34f837bc3e894d90956
dilshan.d3 <dilshan72404@gmail.com>
2024-06-05 17:19:54 +0530

    basic server example

--- part85/server.js
+++ part85/server.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+import express from 'express'  //this is a basic server example
+const app = express()
+const port = 3000
+
+app.get("/",(req,res) => {
+    res.send(" dilshan u done it")
+})
+
+app.get("/home",(req,res) => [
+    res.send("dilshan your done it")
+])
+
+app.listen(port,() => [
+    console.log(`the url is https://localhost:${port}`)
+])
